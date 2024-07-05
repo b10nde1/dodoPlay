50b88d2e95dc89b7af92e086b797e39ded25feae
tarun.mungurah <tarun.mungurah@MAEGL2VXB>
2024-07-05 14:30:57 +0400

    feat(form2): TextArea now working and UnitTest1.cs is changed to Form2.cs

--- PlaywrightTests/MyUtitilies.cs
+++ PlaywrightTests/MyUtitilies.cs
@@ -4,4 +4,7 @@ public class MyUtitilies
 {
     public string GenerateID()
         => DateTime.Now.ToString("yyyyMMddHHmmssffff");
+
+    public string SelectFromList(string [] list)
+        => list[new Random().Next(list.Length)];
 }
--- playwrightForm2/PlaywrightTests/MyUtitilies.cs
+++
@@ -1,7 +0,0 @@
-namespace PlaywrightTests;
-
-public class MyUtitilies
-{
-    public string GenerateID()
-        => DateTime.Now.ToString("yyyyMMddHHmmssffff");
-}
